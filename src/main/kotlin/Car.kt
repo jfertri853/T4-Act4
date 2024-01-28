a6fa661869ba568b92be7d2d20690d9737877495
jfertri853 <ftjulio@iesrafaelalberti.es>
2024-01-28 21:47:02 +0100

    documentación mejorada

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -30,36 +30,48 @@ class Car(color: String, brand: String, model: String, horsePower: Int, doorNum:
     }
 
     /** Verifica que el color no sea una cadena vacía o en blanco
+     *
+     * @param color
      */
     private fun verifyColor(color: String) {
         require(color.trim().isNotEmpty()) {"La propiedad color no puede estar vacía"}
     }
 
     /** Verifica que la marca no sea una cadena vacía o en blanco
+     *
+     * @param brand
      */
     private fun verifyBrand(brand: String) {
         require(brand.trim().isNotEmpty()) {"La marca no puede estar vacía"}
     }
 
     /** Verifica que el modelo no sea una cadena vacía o en blanco
+     *
+     * @param model
      */
     private fun verifyModel(model: String) {
         require(model.trim().isNotEmpty()) {"El modelo no puede estar vacío"}
     }
 
     /** Verifica que el numero de caballos esté entre 70 y 700
+     *
+     * @param horsePower
      */
     private fun verifyHorsePower(horsePower: Int) {
         require(horsePower in 70..700) {"El número de caballos no puede ser menor de 70 ni mayor de 700"}
     }
 
     /** Verifica que el numero de puertas esté entre 3 y 5
+     *
+     * @param doorNum
      */
     private fun verifyDoorNum(doorNum: Int) {
         require(doorNum in 3..5) {"El número de puertas no puede ser menor de 3 ni mayor de 5"}
     }
 
     /** Verifica que la matrícula tenga 7 caracteres
+     *
+     * @param licensePlate
      */
     private fun verifyLicensePlate(licensePlate: String) {
         require(licensePlate.trim().length == 7) {"La matrícula debe tener 7 caracteres"}
